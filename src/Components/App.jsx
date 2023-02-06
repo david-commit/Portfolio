03a578b603378d7d26281d277b1bc153a73f9be4
david-commit <davidondiege@gmail.com>
2023-02-06 04:37:37 +0300

    Created navbar

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,10 +4,16 @@ import NavBar from './NavBar'
 function App() {
 
   return (
-    <div className="App">
+    <div
+      className='App'
+      data-bs-spy='scroll'
+      data-bs-target='#toggleMobileMenu'
+      data-bs-offset='0'
+      tabindex='0'
+    >
       <NavBar />
     </div>
-  )
+  );
 }
 
 export default App
