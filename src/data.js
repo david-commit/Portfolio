4dc9bcecf501e3f70b9a35525e81c4ce29fd8479
david-commit <davidondiege@gmail.com>
2023-02-16 09:43:06 +0300

    Compressed images

--- src/data.js
+++ src/data.js
@@ -68,56 +68,56 @@ const skills = [
 const projects = [
   {
     name: 'Newlife Hospital',
-    image: 'https://i.postimg.cc/V67rcGGT/newlife.png',
+    image: 'https://i.postimg.cc/Vkg8PNyD/newlife.png',
     url: 'https://newlife-frontend.vercel.app/',
     description:
       'Newlife is a community in which all people achieve their full potential for health and well-being across the lifespan. We work to be trusted by patients, a valued partner in the community, and creators of positive change.',
   },
   {
     name: 'iCare Caregiving',
-    image: 'https://i.postimg.cc/JhYDfYdF/iCare.png',
+    image: 'https://i.postimg.cc/fRB6ggXP/iCare.png',
     url: 'http://i-care-frontend.vercel.app/',
     description:
       'We Help Families Care for their Loved Ones iCare provides home health care services and supports so that every person may live a fulfilled life in their home and community regardless of disability.',
   },
   {
     name: 'Vermont Flowers EPZ',
-    image: 'https://i.postimg.cc/Y0MHbMzB/vermont.png',
+    image: 'https://i.postimg.cc/13QmZT85/vermont.png',
     url: 'https://www.vermont-design.com/',
     description:
       'Vermont Flowers EPZ Ltd is the leading preserved flower company in Kenya. They offer customer specific and customised preserved flowers, foliage and floral arrangements for all occasions. Their products are guaranteed to last for a long time.',
   },
   {
     name: 'Fitness Palace',
-    image: 'https://i.postimg.cc/YCp3N9Gz/fitness-palace.png',
+    image: 'https://i.postimg.cc/J4QgGdTh/fitness-palace.png',
     url: 'https://fitness-palace-frontend-ten.vercel.app/',
     description:
       'At Fitness Palace we take pride in enabling our clients achieve their fitness goals one step at a time. Take a look at some of the features we offer.',
   },
   {
     name: 'GymIT',
-    image: 'https://i.postimg.cc/W1Pnszd1/gymit.jpg',
+    image: 'https://i.postimg.cc/g01tC90g/gymit.jpg',
     url: 'https://gym-it.vercel.app/',
     description:
       'GymIT Gym is a platform users can look to improve their physical fitness by providing trainer options. Also, a user can register as a trainer.',
   },
   {
     name: 'PataBoda',
-    image: 'https://i.postimg.cc/XqbFVH9k/pataboda.png',
+    image: 'https://i.postimg.cc/vH1gmp8r/pataboda.png',
     url: 'https://david-commit.github.io/PataBoda/',
     description:
       'PataBoda is an asset financing platform that provides loans to entrepreneurs and low-income earners, thus enabling them to acquire bodabodas with affordable down payments and low interest rates.',
   },
   {
     name: "Pixie's Hair Saloon",
-    image: 'https://i.postimg.cc/qqJRb98S/saloon.png',
+    image: 'https://i.postimg.cc/FR3vqVhJ/saloon.png',
     url: 'http://saloon-sigma.vercel.app/',
     description:
       "Pixie's Hair Saloon is a platform that reccomends the most prominent lady hairstles with their descriptions, pricing and success rate. A user can also add their preferred styles",
   },
   {
     name: 'Microsoft UI Clone',
-    image: 'https://i.postimg.cc/fT4Zb4tN/microsoft.pngl',
+    image: 'https://i.postimg.cc/QCKTvPHJ/microsoft.png',
     url: 'https://microsoft-ui-clone.pages.dev/',
     description:
       "Just a tutorial demo clone of Microsoft's website teaching the basics of HTML, CSS and git using Vite + ReactJs. Deployed on CloudFlare pages.",
